e1ee7e7d972e3bbb383e0bbdbb347e1c526b4ab7
Nando Davis <nandodavis@gmail.com>
2023-02-10 12:27:36 +0100

    Fix pointer issue with not managed stores

--- internal/renderer_test.go
+++ internal/renderer_test.go
@@ -97,3 +97,52 @@ func TestRenderResourcesStores(t *testing.T) {
 
 	assert.Contains(t, data, `client_secret = data.sops.values["my-secret"]`)
 }
+
+
+func TestRenderResourcesStoresWithManagedFalse(t *testing.T) {
+	trueRef := true
+	cfg := &SiteConfig{
+		ProjectKey:   "key",
+		ClientSecret: "test",
+		ProjectSettings: &CommercetoolsProjectSettings{
+			Countries: []string{"NL", "DE"},
+		},
+		Frontend: &CommercetoolsFrontendSettings{
+			CreateCredentials: &trueRef,
+		},
+		TaxCategories: []CommercetoolsTaxCategory{
+			{
+				Key:  "low",
+				Name: "Low Tax",
+				Rates: []CommercetoolsTax{
+					{
+						Country:         "NL",
+						Amount:          0.8,
+						Name:            "Low",
+						IncludedInPrice: &trueRef,
+					},
+				},
+			},
+		},
+		Stores: []CommercetoolsStore{
+			{
+				Key: "my-store",
+				Managed:  &[]bool{false}[0], // Create bool pointer
+			},
+		},
+		Zones: []CommercetoolsZone{
+			{
+				Name:        "Primary",
+				Description: "Primary zone",
+				Locations: []CommercetoolsZoneLocation{
+					{
+						Country: "NL",
+					},
+				},
+			},
+		},
+	}
+	data, err := renderResources(cfg, "0.1.0")
+	require.NoError(t, err)
+	assert.NotContains(t, data, `depends_on = [commercetools_store.my-store]`)
+}
